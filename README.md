1b4a856344f937167f0fe650fa6a2bfc34a38c80
Nobutaka Wakabayashi <wakabayashi@digitalsheep.co.jp>
2014-08-05 09:58:22 +0900

    配置変更 && 注意文など追加

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-workshop-2014-03
-================
+# Perl入学式2014年度第3回の資料
 
-2014年度第3回の資料
+`slide.md`が資料本体です。
+
+資料は Markdown 記法で書かれていますので、テキストエディタだけでなく、Markdownエディタなどでもご覧いただけます。
+
+また、`revealup`に対応していますので、スライドとして閲覧も可能です。スペースキーで順番に閲覧できます。
+
+    $ revealup serve slide.md
+
+上記コマンドが動作しない場合は、`App::revealup`をインストールしてください。
+
+    $ cpanm App::revealup
+
+## 編集方法
+
+`src`ディレクトリのなかに各章ごとのファイルがありますので、そちらを編集してください。
+
+編集後には`build.pl`を実行して`slide.md`を生成してください。
+
+    $ perl build.pl
+
+Perl入学式公式サイト： <http://www.perl-entrance.org>
--- build.pl
+++ build.pl
@@ -5,10 +5,23 @@ use utf8;
 
 use Path::Tiny;
 
-my @slides = qw/ intro.md array-func.md qw.md hash.md hash-func.md reference.md /;
-my $slide = join("\n---\n", map {
-    my $str = path($_)->slurp_utf8;
-    $str =~ s/\n(##?)/\n___\n$1/g;
-    $str
-} @slides);
-path("slide.md")->spew_utf8($slide);
+my $src_dir = './src';
+my $dst_dir = '.';
+my @slides  = qw/
+  intro.md
+  array-func.md
+  qw.md hash.md
+  hash-func.md
+  reference.md
+  /;
+my $slide
+  = "<!-- このファイルは直接編集せずに src ディレクトリ内を編集し build.pl を実行してください。 -->\n";
+$slide .= join(
+    "\n---\n",
+    map {
+        my $str = path("$src_dir/$_")->slurp_utf8;
+        $str =~ s/\n(##?)/\n___\n$1/g;
+        $str
+    } @slides
+);
+path("$dst_dir/slide.md")->spew_utf8($slide);
