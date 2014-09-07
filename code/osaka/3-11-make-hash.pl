adb6781899d3ce6e671ff19c374142d084a72829
azumakuniyuki <azumakuniyuki@gmail.com>
2014-09-07 15:04:34 +0900

    Add answer script for /#/3/11

---
+++ code/osaka/3-11-make-hash.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my %hash = (
+    'name' => 'azumakuniyuki',
+    'sex'  => 'Ossan',
+    'food' => 'Tofu'
+);
+
+use Data::Dumper;
+print Dumper( %hash );
+
