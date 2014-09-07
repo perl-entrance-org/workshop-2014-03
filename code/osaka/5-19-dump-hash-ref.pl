e75f05013cb8b2d78fca5e39343b22038a84a16f
azumakuniyuki <azumakuniyuki@gmail.com>
2014-09-07 15:27:46 +0900

    Add answer script for /#/5/19

---
+++ code/osaka/5-19-dump-hash-ref.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $papix = {
+    name    => 'papix',
+    address => 'Tokyo',
+    age     => 25,
+};
+
+print Dumper( $papix );
+
