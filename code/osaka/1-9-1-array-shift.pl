b0d24a029952ff9f3bc43c3f59b69327d26da41f
azumakuniyuki <azumakuniyuki@gmail.com>
2014-09-07 15:51:31 +0900

    Merged into 1-9-1-array-push-shift.pl

--- code/osaka/1-9-1-array-shift.pl
+++ code/osaka/1-9-1-array-shift.pl
@@ -2,8 +2,24 @@
 use strict;
 use warnings;
 
+my @array = ();
+my $value = '';
+
 # Q1
-my @array = ( 'Alice', 'Bob' );
-my $value = shift @array;
+@array = ( 'Alice', 'Bob' );
+$value = shift @array;
 print "$value\n";
+
+# Q2
+@array = ( 'Alice', 'Bob' );
+$value = pop @array;
+print "$value\n";
+
+# Q3
+@array = ( 'Alice', 'Bob' );
+unshift @array, 'Zappa';
+print "@array\n";
+
+push @array, 'Chris';
+print "@array\n";
 
