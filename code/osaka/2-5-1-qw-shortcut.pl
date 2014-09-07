119767a65608999a77593ed6b08f9166b1cfd068
azumakuniyuki <azumakuniyuki@gmail.com>
2014-09-07 15:55:05 +0900

    Merged into 2-5-1-qw-shortcut.pl

--- code/osaka/2-5-1-qw-shortcut.pl
+++ code/osaka/2-5-1-qw-shortcut.pl
@@ -2,16 +2,18 @@
 use strict;
 use warnings;
 
+# Q1
 my @x = qw/0120 123 XXX/;
 print "@x\n";
 
+# Q2
 my $y = join '-', @x;
+print "$y\n";
 
+# Q3
 my @z = split '/', '/usr/bin/env perl';
 #my @z = split /\//, '/usr/bin/env perl';
-
-print "Q1. @x\n";
-print "Q2. $y\n";
+print "@z\n"
 
 my $c = 0;
 # shift @z;
